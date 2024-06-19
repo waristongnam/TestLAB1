313fcbe2eace8b680da41564ebe7c7f19aa5ddaa
waristongnam <waris.t@kkumail.com>
2024-06-19 16:29:28 +0700

    Update TEST.java

--- TEST.java
+++ TEST.java
@@ -1,5 +1,12 @@
-public int x = 10;
-public int y = 5;
-
-public int z = x + y;
-system.out.print(z)
+public class MyClass { 
+    public static void main(String[] args) {
+        float x = 1;
+        float y = 2;
+        float z;
+        for(int i = 0;i<6;i++){
+            z = (x+(y/x))/2;
+            System.out.println(z);
+            x = z;
+        }
+    }
+}
